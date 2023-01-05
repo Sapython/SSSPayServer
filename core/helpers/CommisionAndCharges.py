135f039511a6289862bc8c4c3ca6a283f2054d88
Saptam <saptampro2003@gmail.com>
2023-01-06 00:49:49 +0530

    Update CommisionAndCharges.py

--- core/helpers/CommisionAndCharges.py
+++ core/helpers/CommisionAndCharges.py
@@ -36,62 +36,48 @@ class CommissionAndCharges:
 
     def setCommission(self, transactionData, userId, transactionId):
         # print("Commission Data: ",transactionData)
+        access = ['admin', 'superDistributor', 'masterDistributor',
+                  'distributor', 'retailer', 'guest']
         members = self.fs.collection('groups').document(
             transactionData['groupId']).get()
         # print(self.commissions)
-        isCommission = False
         if (transactionData['serviceType'] in ['dth', 'mobile_recharge', 'aeps']):
             result = list(sorted(list(filter(
                 lambda x: x['service'] == transactionData['serviceType'], self.commissions)), key=lambda y: y['minimumAmount']))
-            # print("Commission",result)
-            isCommission = True
-        if (not isCommission):
-            return "No Commission"
-        # elif (transactionData['serviceType'] in ['expressPayoutUpi','expressPayoutImps','payoutImps','payoutUPI']):
-        #     result = list(sorted(list(filter(lambda x: x['service'] == transactionData['serviceType'], self.charges)),key=lambda y:y['minimumAmount']))
-        #     print("Charge",result)
-        # else:
-        #     print("No Commission")
-        #     return "No ADDITIONAL FOUND"
         finalRes = {}
-        # print("result",result)
-        # transactionData = self.fs.collection('users').document(userId).collection('transaction').document(transactionID).get().to_dict()
         for res in result:
-            # print("COND",res['maximumAmount'],transactionData['amount'],res['minimumAmount'],(res['maximumAmount'] >= transactionData['amount']),( transactionData['amount'] >= res['minimumAmount']))
             if((res['maximumAmount'] >= transactionData['amount']) and (transactionData['amount'] >= res['minimumAmount'])):
                 finalRes = res
                 break
+        
         try:
             res = finalRes['accessLevels']
         except:
             print("No Commission charges set for price range")
             return "No Commission charges set for price range"
         charges = []
-        # print("finalRes",finalRes)
-        if(finalRes):
-            for accesses in finalRes['accessLevels']:
-                if(finalRes['type'] == 'percentage'):
-                    charges.append(
-                        (finalRes[accesses]/100)*transactionData['amount'])
-                elif finalRes['type'] == 'fixed':
-                    charges.append(finalRes[accesses])
-        totalCharge = 0
-        print("---------------------------")
-        commissions = []
-        # get current member using userId
+        # # print("finalRes",finalRes)
+        # # get current member using userId
+        access = finalRes['accessLevels']
         user = list(filter(lambda x: x['id'] ==
                     userId, members.to_dict()['members']))
-        print("user", user)
-        access = ['admin', 'superDistributor', 'masterDistributor',
-                  'distributor', 'retailer', 'guest']
         validAccess = access[0:access.index(user[0]['access']['access'])+1]
+        if(finalRes):
+            for accesses in validAccess:
+                if(finalRes['type'] == 'percentage'):
+                    charges.append({
+                        "access":accesses,
+                        "amount":(transactionData['amount']/100)*finalRes[accesses]
+                    })
+                elif finalRes['type'] == 'fixed':
+                    charges.append({
+                        "access":accesses,
+                        "amount":finalRes[accesses]
+                    })
+        commissions = []
         for member in members.to_dict()['members']:
-            # print("Member",member)
-            if(member['access']['access'] in finalRes['accessLevels']):
-                # print()
-                amount = math.floor(
-                    charges[finalRes['accessLevels'].index(member['access']['access'])])
-                print(amount, userId)
+            if(member['access']['access'] in validAccess):
+                amount = charges[validAccess.index(member['access']['access'])]['amount']
                 if (amount > 0):
                     if len(list(filter(lambda x: x['member'] == member['id'], commissions))) == 0:
                         if ((member['access']['access'] == 'retailer' and member['id'] == userId) or member['access']['access'] != 'retailer') and member['access']['access'] in validAccess:
@@ -101,18 +87,18 @@ class CommissionAndCharges:
                                 "name": member['displayName'],
                                 "access": member['access']['access']
                             })
-                            totalCharge += amount
+        self.fs.collection('users').document(userId).collection('transaction').document(transactionId).update({
+            "commissions": firestore.ArrayUnion(commissions)
+        })
         for commission in commissions:
             self.fs.collection('users').document(userId).collection('wallet').document('wallet').update({
                 'balance': firestore.Increment(amount)
-            })
-            self.fs.collection('users').document(userId).collection('transaction').document(transactionId).update({
-                "commissions": firestore.ArrayUnion([commission])
             })
             self.fs.collection('users').document(commission['member']).collection('commissions').add({
                 **transactionData,
                 'exchangeAmount': commission['amount'],
                 'member': commission['member'],
+                "name": commission['name'],
                 "ownerId": userId,
                 'transactionType': 'debit',
                 'transactionTime': firestore.SERVER_TIMESTAMP
